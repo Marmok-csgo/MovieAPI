ad75907fd8b7986ea8d48bc93bcd3c9a9228ee77
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-10-26 13:13:34 +0500

    Migrations

---
+++ MovieAPI/Migrations/20231025054832_CountryIdFieldAddedToMovieClass.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountryIdFieldAddedToMovieClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CountryId",
+                table: "Movies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CountryId",
+                table: "Movies",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+        }
+    }
+}
