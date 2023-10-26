d11c4b7608c331e2641c31a8d2a4c6ffa8d4f724
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-10-26 13:13:10 +0500

    Migrations

---
+++ MovieAPI/Migrations/20231024124308_CountryAdded.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CountryAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Movies",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryId",
+                table: "Movies",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movies",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CountryId",
+                table: "Movies",
+                column: "CountryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Countries_CountryId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_CountryId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CountryId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movies");
+        }
+    }
+}
---
+++ MovieAPI/Migrations/20231025053657_DescriptionFieldAddedToMovieClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DescriptionFieldAddedToMovieClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Movies");
+        }
+    }
+}
