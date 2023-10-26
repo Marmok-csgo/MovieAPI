67d6734bc4006e0950ac87aa856bb585f6828509
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-10-26 17:48:06 +0500

    Response

---
+++ MovieAPI/Responses/MovieResponse.cs
@@ -0,0 +1,26 @@
+namespace MovieAPI.Responses;
+
+public class MovieResponse
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? CountryName { get; set; }
+    
+    public DateOnly RealeseDate { get; set; }
+    
+    public string? Poster { get; set; }
+    
+    public string? Author { get; set; }
+
+    public MovieResponse(int id, string? name, string? description, string? countryName, DateOnly realeseDate, string? poster, string? author)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        CountryName = countryName;
+        RealeseDate = realeseDate;
+        Poster = poster;
+        Author = author;
+    }
+}
