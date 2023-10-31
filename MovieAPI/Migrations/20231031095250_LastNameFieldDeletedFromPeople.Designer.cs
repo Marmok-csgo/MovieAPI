12c48c317950d4338b35e23e7a18c3d52613160a
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-10-31 15:38:07 +0500

    migrations

--- MovieAPI/Migrations/20231031095250_LastNameFieldDeletedFromPeople.Designer.cs
+++ MovieAPI/Migrations/20231031095250_LastNameFieldDeletedFromPeople.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieAPI.Models;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MovieAPI.Migrations
 {
     [DbContext(typeof(MovieContext))]
-    partial class MovieContextModelSnapshot : ModelSnapshot
+    [Migration("20231031095250_LastNameFieldDeletedFromPeople")]
+    partial class LastNameFieldDeletedFromPeople
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MovieAPI/Migrations/20231031095250_LastNameFieldDeletedFromPeople.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class LastNameFieldDeletedFromPeople : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "People");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "People",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "People",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "People",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
