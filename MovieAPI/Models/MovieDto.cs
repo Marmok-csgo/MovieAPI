bac10252dd08edfd5d6832cae257c2c20b5073d2
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-11-06 13:59:30 +0500

    Models

--- MovieAPI/Models/MovieDto.cs
+++ MovieAPI/Models/MovieDto.cs
@@ -1,19 +1,27 @@
+using Microsoft.Build.Framework;
+
 namespace MovieAPI.Models;
 
 public class MovieDto
 {
-    public string? Name { get; set; }
-    
-    public string? Description { get; set; }
-    
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Description { get; set; }
+
+    [Required]
     public int CountryId { get; set; }
-    
-    public List<int> GenresIds { get; set; } = null!;
 
-    public List<int> PeopleIds { get; set; } = null!;
+    [Required]
+    public List<int> GenresIds { get; set; }
 
+    [Required]
+    public List<int> PeopleIds { get; set; }
+
+    [Required]
     public DateOnly ReleaseDate { get; set; }
-    
-    public IFormFile? Poster { get; set; }
-    
+
+    [Required]
+    public IFormFile Poster { get; set; }
 }
