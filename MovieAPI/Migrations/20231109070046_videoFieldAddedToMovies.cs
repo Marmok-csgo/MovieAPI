8b295d14601120580b2f18fa0bc09d3f26dc056d
MrAke <131134887+Marmok-csgo@users.noreply.github.com>
2023-11-09 15:37:50 +0500

    Migrations

---
+++ MovieAPI/Migrations/20231109070046_videoFieldAddedToMovies.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class videoFieldAddedToMovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Video",
+                table: "Movies",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Video",
+                table: "Movies");
+        }
+    }
+}
